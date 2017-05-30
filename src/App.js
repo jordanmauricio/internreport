9e4a460258f7a21aac67968919734e2de3bd1e24
Jordan Mauricio <jordanmauricio1@gmail.com>
2017-05-30 15:14:33 +0200

    A lot of smalle stuff + NOW integration from Zeit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 class App extends Component {
     render() {
         return (
-            <div className="content">
+            <div className="container content">
                 <Header />
                 <Main />
             </div>
--- src/main/Home.jsx
+++ src/main/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Home</h1>
                 <p>Animation through transitions where the website goes from no-styling to looking nice.</p>
                 <Link to="/introduction">Go To introduction</Link>
