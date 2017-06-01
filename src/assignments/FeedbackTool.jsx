8820d0c1033aec1570fb039861de59ae6e15b154
Jordan Mauricio <jordanmauricio1@gmail.com>
2017-06-01 10:51:53 +0200

    Julian typography help

--- src/assignments/FeedbackTool.jsx
+++ src/assignments/FeedbackTool.jsx
@@ -23,11 +23,11 @@ class FeedbackTool extends Component {
                     <li>Express</li>
                 </ul>
                 <p>As you, dear reader, might realize: the technology set here is very similar to that of Unicarriers (apart from Meteor), but then a backend system added on top of it (Firebase, Node, Express etc.) With Michiel’s initial prototype in hand, I had jumped in to build up this tool. I must add at this point, that by the end of it, the initial prototype was pretty much no where to be seen.</p>
-                <p><strong>How It Went</strong></p>
+                <h3>How It Went</h3>
                 <p>Peter Bakker, the Product Owner for the project, had helped me alongside Michiel to create my User Stories and Scrum board, which streamlined the project’s flow. I had set out to build the foundation first, with no design at all on top. After approximately 3 weeks, I had the core of the application working, without having written any CSS or styling. It is around this time, that Sarina had joined to help me design and build the actual design of the tool. </p>
                 <p>This tool caused several headaches, but also several celebratory Pepsi’s. I have learned a lot building this project from scratch. Not just programming structures and design patterns, but at certain points I had to do a complete 180 on the way I was thinking, go back and restructure a significant portion of my code, before being able to continue.</p>
                 <p>The most important takeaway from this project must be planning ahead. As explained before, I’ve had to restructure and rebuild the application before I continued, this happened twice on a large scale, and a handful of times on a smaller scale. This came about because in the beginning (where of this happened) I was focusing on each individual component of the application, instead of focusing on the bigger picture, the application as a whole. In order to prevent this, I have decided that if an application’s scope seems to big to keep in your thoughts, it’s a very good idea to simply write out your logical structures and flow down on paper and analyzing that before continuing. Sometimes it might be better to calmly think through the problem for a day and save yourself a lot of time down the line, instead of jumping it and creating components immediately.</p>
-                <div className="button"><p><Link to="/assignments/internshipreport">Internship Report</Link></p></div>
+                <Link to="/assignments/internshipreport"><div className="button"><p>Internship Report</p></div></Link>
             </div>
         );
     }
