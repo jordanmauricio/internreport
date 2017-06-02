d68bca09545b1b585cb25eb6656251b58edc03ae
Jordan Mauricio <jordanmauricio1@gmail.com>
2017-06-02 10:50:15 +0200

    Spelling fix

--- src/main/Home.jsx
+++ src/main/Home.jsx
@@ -6,7 +6,7 @@ class Home extends Component {
         return (
             <div>
                 <h1 className="center">Home</h1>
-                <p>This page will display a short 10sec intro where the page will go from a plain HTML with no styling, to various iterations and ends up looking very similiar to how the page looks like now.</p>
+                <p>This page will display a short 10 second intro where the page will go from a plain HTML with no styling, to various iterations and ends up looking very similiar to how the page looks like now.</p>
 
                 <div className="start-button has-text-centered">
                     <div className="button is-primary is-outlined">
