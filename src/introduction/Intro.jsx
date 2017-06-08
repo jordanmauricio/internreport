e5d6c5fc76dcb7401c90ae09a73d990ad7f96288
Jordan Mauricio <jordanmauricio1@gmail.com>
2017-06-08 12:40:30 +0200

    working on video header

--- src/introduction/Intro.jsx
+++ src/introduction/Intro.jsx
@@ -7,12 +7,15 @@ class Intro extends Component {
         return (
             <div className="page--with-video-header">
                  <div className="video-header">
-                    <video playsInline autoPlay muted loop id="home-video">
-                        <source src={video} type="video/mp4" />
-                    </video>
+                        {/*<div className="overlay">*/}
+                            <video playsInline autoPlay muted loop id="home-video">
+                                <source src={video} type="video/mp4" />
+                            </video>
+                        {/*</div>*/}
                     <div className="video-header__content">
                         <h1>Welcome,</h1>
                     </div>
+                    <span></span><span></span>
                 </div>
                 <div className="shrinkwrap">
                     <p>This report has been written as part of my time as an intern at TRIMM. From February 6th until June 30 I have been working as a front-end developer primarily, and back-end developer when possible with the E-commerce team. During my time there I have worked on some terrific projects and met some great people. My team was more than happy to help me whenever I requested it, and I couldn’t have made my time there as rewarding as it was without their help.</p>
